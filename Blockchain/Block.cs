527d8ea4f5e91215ad11fa13ca72280fa236c88b
Bruno Roque <bruno.roque@celfinet.com>
2017-06-28 23:39:27 +0100

    Block class added

---
+++ Blockchain/Block.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Blockchain
+{
+    public class Block
+    {
+        int Index;
+        byte[] PreviousHash;
+        DateTime Timestamp;
+        double Data;
+        byte[] Hash;
+
+        public Block(int index, byte[] previousHash, DateTime timestamp, double data, byte[] hash)
+        {
+            Index = index;
+            PreviousHash = previousHash;
+            Timestamp = timestamp;
+            Data = data;
+            Hash = hash;
+        }
+    }
+}
