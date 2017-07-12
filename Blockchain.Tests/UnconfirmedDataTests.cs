c03851e33e844ee8e6014e364cf31a8614eb524d
Bruno Roque <bruno.roque@celfinet.com>
2017-07-12 23:21:52 +0100

    UnconfirmedData class added

---
+++ Blockchain.Tests/UnconfirmedDataTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace Blockchain.Tests
+{
+    [TestFixture]
+    class UnconfirmedDataTests
+    {
+        [Test]
+        public void Test_get_data_to_return_the_same_value_as_the_added_one()
+        {
+            var unconfirmedData = new UnconfirmedData();
+            var data = "ThisIsData";
+
+            unconfirmedData.AddData(data);
+            var result = unconfirmedData.GetData();
+
+            Assert.That(result, Is.EqualTo(data));
+        }
+
+        [Test]
+        public void Test_get_data_with_an_empty_pile_to_return_null()
+        {
+            var unconfirmedData = new UnconfirmedData();
+
+            var result = unconfirmedData.GetData();
+
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void Test_get_data_to_respect_the_order_of_insertion()
+        {
+            var unconfirmedData = new UnconfirmedData();
+            var data1 = "First";
+            var data2 = "Second";
+
+            unconfirmedData.AddData(data1);
+            unconfirmedData.AddData(data2);
+
+            var result1 = unconfirmedData.GetData();
+            var result2 = unconfirmedData.GetData();
+            var result3 = unconfirmedData.GetData();
+
+            Assert.That(result1, Is.EqualTo(data1));
+            Assert.That(result2, Is.EqualTo(data2));
+            Assert.That(result3, Is.Null);
+        }
+    }
+}
---
+++ Blockchain/UnconfirmedData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Blockchain
+{
+    public class UnconfirmedData
+    {
+        private List<string> _datas;
+
+        public UnconfirmedData()
+        {
+            _datas = new List<string>();
+        }
+
+        public void AddData(string data)
+        {
+            _datas.Add(data);
+        }
+
+        public string GetData()
+        {
+            string data = null;
+
+            if (_datas.Count > 0)
+            {
+                data = _datas[0];
+                _datas.RemoveAt(0);
+            }
+
+            return data;
+        }
+    }
+}
