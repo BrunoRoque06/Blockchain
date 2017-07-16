5a9adfdb8a31bf403fc1d523df4ee36f66f4b92e
Bruno Roque <bruno.roque@celfinet.com>
2017-07-16 12:44:52 +0100

    Blockchain printer updated

--- Blockchain/BlockchainPrinter.cs
+++ Blockchain/BlockchainPrinter.cs
@@ -22,10 +22,10 @@ namespace Blockchain
             Console.WriteLine("\nMiner Blockchain:");
             Console.Write("# {0, 3}", "Ind");
             Console.Write("{0, 15}", "Data");
-            Console.Write("{0, 7}", "Nonce");
-            Console.Write("{0, 12}", "Hash");
-            Console.Write("{0, 12}", "PreviousHash");
-            Console.Write("{0, 20}", "DateTime");
+            Console.Write("{0, 10}", "Nonce");
+            Console.Write("{0, 15}", "Hash");
+            Console.Write("{0, 15}", "PreviousHash");
+            Console.Write("{0, 22}", "DateTime");
             Console.Write("\n");
         }
 
@@ -33,10 +33,10 @@ namespace Blockchain
         {
             Console.Write("# {0, 3}", block.Index);
             Console.Write("{0, 15}", block.Data);
-            Console.Write("{0, 7}", block.Nonce);
-            Console.Write("{0, 12}", block.Hash);
-            Console.Write("{0, 12}", block.PreviousHash);
-            Console.Write("{0, 20}", block.DateTime);
+            Console.Write("{0, 10}", block.Nonce);
+            Console.Write("{0, 15}", block.Hash);
+            Console.Write("{0, 15}", block.PreviousHash);
+            Console.Write("{0, 22}", block.DateTime);
             Console.Write("\n");
         }
     }
